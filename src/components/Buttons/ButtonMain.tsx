62d593d13354591f7f26d09250ef71bc78c5d06d
Hugo Lansade <lhugo@lansade.com>
2022-01-16 18:17:47 +0100

    modified button css

---
+++ src/components/Buttons/ButtonMain.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ButtonMain() {
+    return (
+        <button className='btn-container'>
+            <span className='btn-text'>Justify</span>
+        </button>
+    )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../App.css"
+
+
+export default function Header() {
+    return (
+        <div className='header-container'>
+            <span className='header-name'>Hugo <br/>Lansade</span>
+            <div className='header-ttt-info'>
+                <img src="./img/ttt-logo.svg" className='logo-ttt' alt="logo-tictactrip" />
+                <span className='ttt-name'>tictactrip</span>
+            </div>
+        </div>
+    )
+}
